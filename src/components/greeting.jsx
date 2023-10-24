64a466e320abecbc13bef78d49969e3f15f97a6b
Andrea Manuel <andreamanuel2207@gmail.com>
2023-10-24 11:31:11 -0600

    Create a greeting component to display the random greeting

---
+++ src/components/greeting.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+function Greeting() {
+  const { greeting } = useSelector((state) => state.greeting)
+  return (<main>
+      <h1>Random greeting</h1>
+      <span>{greeting.text}</span>
+    </main>);
+}
+
+export default Greeting;
