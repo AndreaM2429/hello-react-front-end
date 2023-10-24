6a00b47cca0967850637863746e3414814a2ee73
Andrea Manuel <andreamanuel2207@gmail.com>
2023-10-24 11:32:24 -0600

    Import the greeting component inside the app component and fetch the API information

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Greeting from './components/greeting';
+import { useDispatch } from 'react-redux';
+import { fetchGreeting } from './redux/greeting/GreetingSlice';
+import { useEffect } from 'react';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchGreeting());
+  }, [dispatch]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greeting />
     </div>
   );
 }
